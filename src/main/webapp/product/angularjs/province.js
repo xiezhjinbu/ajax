9cd3dd2bb8a1b46ff7eef43375c8e161af265304
poxiao58 <1782840861@qq.com>
2016-12-22 13:54:31 +0800

    servlet+ajax+jquery+angularjs示例完成

---
+++ src/main/webapp/product/angularjs/province.js
@@ -0,0 +1,25 @@
+//servlet+ajax+jquery+angular实例js
+var app=angular.module('app',[]);
+app.controller('controller', ['$scope',function($scope) {
+	$scope.data='';
+	$scope.name='test';
+	$scope.cityName=[''];
+	
+	$("#province").change(//监听省份下拉菜单选择事件
+			function(){
+				var province=$("#province").val();//得到选择的省份
+				$.ajax({
+					url:"/ajax/angularjsCityServlet",
+					data:{province:province},
+					type:'post',
+				    dataType:"json",  
+					success: function(data){
+						$("#city").show();
+						$scope.cityName=data[0];
+						$scope.$apply();//需要手动刷新 
+					},
+				});
+			}		
+	);
+	$("#city").hide();
+}]);
